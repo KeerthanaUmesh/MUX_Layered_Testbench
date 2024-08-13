73fd785e0ee95e871824afcfdc832e85bfba55ae
Keerthana Umesh <147648970+KeerthanaUmesh@users.noreply.github.com>
2024-08-13 23:34:52 +0530

    Update mux.sv

--- mux.sv
+++ mux.sv
@@ -1,11 +1,8 @@
 //2:1 Multiplexer with inputs a and b, select line sel and output y
-module mux(
-  	input a,b,
-  	input sel,
-  	output y
-);
-  
-assign y = sel? a : b;
- 
+module mux2X1(sel,a,b,out);
+  input a,b,sel;
+  output out ;
+ assign out = sel?b:a;
 endmodule
+  
 
